17a2e853108ec2b6f490c88cf7b800186c80fd9d
Piotr Maslanka <maslanapiotr4@gmail.com>
2024-11-14 19:45:00 +0100

    Added post to chatGPT and prompt.txt

--- src/libraries/sendPrompt.js
+++ src/libraries/sendPrompt.js
@@ -1,5 +1,27 @@
-const sendArticle = () => {
+import axios from 'axios'
 
+const sendPrompt = (articleText, prompt) => {
+
+    const apiKey = process.env.REACT_APP_OPENAI_API_KEY;
+    const endpoint = 'https://api.openai.com/v1/chat/completions';
+
+    axios.post(endpoint,
+        {
+            "model": "gpt-4o-mini",
+            "messeges": [{"role": "user", "content": `${prompt}/n ${articleText}`} ]
+        },
+        {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response=>{
+            return response.data.choices[0].message.content;
+        }).catch(error=>{
+            console.error('Error communicating with OpenAI', error);
+            alert(`Error communicating with OpenAI: ${error}`);
+            return;
+        })
 }
 
-export default sendArticle;
+export default sendPrompt;
